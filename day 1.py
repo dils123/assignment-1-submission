7a18165493fe5b69b15958b984238907f477fcef
dils123 <80335023+dils123@users.noreply.github.com>
2021-03-09 19:54:53 +0530

    day 1 assignment submission

---
+++ day 1.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+x=int(input("enter value 1="))
+y=int(input("enter value 2="))
+print(x**y)
+
+
+# In[9]:
+
+
+lower=int(input("Enter the lower range:"))
+upper=int(input("Enter the upper range:"))
+for i in range (lower,upper+1):
+    if i%3==0 :
+        print(i)
+
+
+# In[ ]:
+
+
+
+
